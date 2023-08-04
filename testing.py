d3ffab1985f0af097ffc9357af7e4198753a00b8
SoumyadeepOSD <opencontainer255@gmail.com>
2023-08-04 07:58:24 +0530

    safety checking and connection ahandling tested

--- testing.py
+++ testing.py
@@ -3,8 +3,7 @@ import streamlit as st
 # connection object
 conn = st.experimental_connection("mongodb", type=MongoDBConnection)
 # cursor object
-query = { "price": { "$gt": 400 } }
-cursor = conn.cursor(db="mydb", col="games", query=query)
-
+query = {}
+cursor = conn.cursor(db="test", col="chats", query={})
 res = conn.showData(cursor=cursor)
-st.dataframe(res)
+print(res)
